e9a0e4c61cf1d7386940db0b3eaa17559913d3bf
Harleyoc1 <harley.oconnor@icloud.com>
2021-02-06 15:27:09 +0000

    Confirm password field and other changes to sign up screen.

--- src/main/java/com/harleyoconnor/casino/menus/SignUpScreen.java
+++ src/main/java/com/harleyoconnor/casino/menus/SignUpScreen.java
@@ -1,6 +1,8 @@
 package com.harleyoconnor.casino.menus;
 
 import com.harleyoconnor.casino.Casino;
+import com.harleyoconnor.casino.CommonFonts;
+import com.harleyoconnor.casino.builders.ButtonBuilder;
 import com.harleyoconnor.casino.builders.LabelBuilder;
 import com.harleyoconnor.casino.users.PasswordHandler;
 import com.harleyoconnor.casino.users.User;
@@ -13,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -25,34 +28,51 @@ public final class SignUpScreen extends MenuScreen {
 
     private TextField usernameField;
     private TextField passwordField;
+    private TextField confirmPasswordField;
     private Label errorLabel;
 
-    public SignUpScreen(Stage stage, Scene scene, Pane previousLayout) {
-        super(stage, scene, previousLayout);
+    public SignUpScreen(Stage stage, Scene scene, MenuScreen previousScreen) {
+        super(stage, scene, previousScreen);
     }
 
     @Override
     protected Pane setupScreen() {
         Pane layout = new StackPane();
 
-        VBox vBox = new VBox(5);
+        // Create a vertical box for content with a fixed width of 300.
+        VBox vBox = InterfaceUtils.fixWidth(new VBox(5), 300);
 
+        final Label titleLabel = LabelBuilder.createLabel().text("Sign Up").font(CommonFonts.TITLE_FONT).wrapText().build();
+
+        // Create fields.
         this.usernameField = TextFieldBuilder.createTextField().placeholder("Username").build();
         this.passwordField = TextFieldBuilder.createPasswordField().placeholder("Password").build();
+        this.confirmPasswordField = TextFieldBuilder.createPasswordField().placeholder("Confirm Password").build();
+
+        // Create error label, for when the user enters details wrong - such as entering an existing username.
         this.errorLabel = LabelBuilder.createLabel().wrapText().build();
 
-        Button signUpButton = new Button("Sign Up");
-        signUpButton.setOnAction(this::onSignUpPres);
+        // Create sign up button - this will attempt to sign the user up based on the entered data.
+        Button signUpButton = ButtonBuilder.createButton().text("Sign Up").onAction(this::onSignUpPress).build();
 
-        vBox.setPadding(new Insets(25));
-        vBox.getChildren().addAll(InterfaceUtils.createVerticalSpacer(), this.usernameField, this.passwordField, signUpButton, this.errorLabel, InterfaceUtils.createVerticalSpacer());
+        // Create sign in button - this will redirect the user to the sign in screen.
+        Button signInButton = ButtonBuilder.createButton().text("Sign In").onAction(this::onSignInPress).build();
 
-        layout.getChildren().addAll(vBox);
+        // Add content to the vertical box.
+        InterfaceUtils.centreElementsVertically(InterfaceUtils.addElementsToPane(vBox, titleLabel, this.usernameField, this.passwordField, this.confirmPasswordField,
+                InterfaceUtils.addElementsToPane(new HBox(), signInButton, InterfaceUtils.createHorizontalSpacer(), signUpButton),
+                this.errorLabel));
+
+        // Set padding for the layout.
+        layout.setPadding(new Insets(25));
+
+        // Add vertical box to the layout in the horizontal centre of the screen.
+        InterfaceUtils.addElementsToPane(layout, InterfaceUtils.centreElementsHorizontally(InterfaceUtils.addElementsToPane(new HBox(), vBox)));
 
         return layout;
     }
 
-    private void onSignUpPres(ActionEvent event) {
+    private void onSignUpPress(ActionEvent event) {
         String username = this.usernameField.getText();
 
         // Make sure they have actually entered a username.
@@ -69,6 +89,12 @@ public final class SignUpScreen extends MenuScreen {
 
         String password = this.passwordField.getText();
 
+        // Make sure the password is the same as text in the password confirm.
+        if (!password.equals(this.confirmPasswordField.getText())) {
+            this.errorLabel.setText("Passwords do not match.");
+            return;
+        }
+
         // Make sure the password is at least the minimum length.
         if (password.length() < Users.MIN_PASSWORD_LENGTH) {
             this.errorLabel.setText("Password must have at least " + Users.MIN_PASSWORD_LENGTH + " characters.");
@@ -82,6 +108,15 @@ public final class SignUpScreen extends MenuScreen {
 
         Users.register(user); // Register the user.
         Casino.getInstance().setCurrentUser(user); // Set new user to current one.
+    }
+
+    private void onSignInPress (ActionEvent event) {
+        if (this.previousScreen instanceof SignInScreen)
+            this.previousScreen.show();
+        else {
+            // Create a new sign in screen if it wasn't the previous screen.
+            new SignInScreen(this.stage, this.scene, this);
+        }
     }
 
     @Override
