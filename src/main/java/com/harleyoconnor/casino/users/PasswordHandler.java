2e4078598e79e302512b07dd08d7f0d846c1a2dc
harleyoconnor <harleyoc1@gmail.com>
2021-02-12 18:54:48 +0000

    Fixed password authentication.

--- src/main/java/com/harleyoconnor/casino/users/PasswordHandler.java
+++ src/main/java/com/harleyoconnor/casino/users/PasswordHandler.java
@@ -6,6 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 /**
@@ -16,9 +17,8 @@ import java.util.Base64;
 public final class PasswordHandler {
 
     private static final String HASH_ALGORITHM = "PBKDF2WithHmacSHA1";
-    private static final int SIZE = 16;
+    private static final int SIZE = 128;
     private static final int COST = 65536;
-    private static final int KEY_LENGTH = 128;
 
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     private static final SecretKeyFactory KEY_FACTORY;
@@ -38,40 +38,64 @@ public final class PasswordHandler {
         this.password = password;
     }
 
+    /**
+     * Hashes the the password stored by this {@link PasswordHandler}.
+     *
+     * @return This {@link PasswordHandler}.
+     */
     public PasswordHandler hash () {
         // Create the salt object, with the amount of bytes set to the size constant.
-        byte[] salt = new byte[SIZE];
+        byte[] salt = new byte[SIZE / 8];
 
         // Generate random bytes for the salt.
         SECURE_RANDOM.nextBytes(salt);
-        System.out.println(Base64.getEncoder().encodeToString(salt));
 
-        // Generate the hash from the password char array and random salt.
-        byte[] hash = this.generateHash(this.password.toCharArray(), salt);
+        // Generate the hashed string from the password char array and random salt.
+        byte[] hashedString = this.generateHash(this.password.toCharArray(), salt);
+
+        byte[] hash = new byte[salt.length + hashedString.length];
+
+        // Copy the salt and hashedString to the hash.
+        System.arraycopy(salt, 0, hash, 0, salt.length);
+        System.arraycopy(hashedString, 0, hash, salt.length, hashedString.length);
 
         // Use Base64 encoder to store the bytes as a string.
         this.password = Base64.getEncoder().encodeToString(hash);
+
         return this;
     }
 
+    /**
+     * Authenticates the given password from the hash stored.
+     *
+     * @param passwordIn The password inputted.
+     * @return True if the password matches the hash stored, false if not.
+     */
     public boolean authenticate (String passwordIn) {
-//        // Decode the Base64 string into bytes.
-//        byte[] hash = Base64.getDecoder().decode(this.password);
-//        byte[] salt = Arrays.copyOfRange(hash, 0, SIZE);
-//        byte[] hashFromIn = this.generateHash(passwordIn.toCharArray(), salt);
-//
-//        int zero = 0;
-//        for (int idx = 0; idx < hashFromIn.length; ++idx) {
-//            zero |= hash[SIZE + idx] ^ hashFromIn[idx];
-//        }
-//
-//        return zero == 0;
-        return true; // Temporary until this is fixed.
+        // Decode the Base64 string into bytes.
+        byte[] hash = Base64.getDecoder().decode(this.password);
+
+        // Get the salt from the hash.
+        byte[] salt = Arrays.copyOfRange(hash, 0, SIZE / 8);
+
+        // Generate a new hash from the salt stored and password input.
+        byte[] hashFromIn = this.generateHash(passwordIn.toCharArray(), salt);
+
+        // Check the hashes match.
+
+        int zero = 0;
+        for (int idx = 0; idx < hashFromIn.length; ++idx) {
+            // Adds bits to "zero" if any were left over from an "OR" operation on the two hashes.
+            zero |= hash[salt.length + idx] ^ hashFromIn[idx];
+        }
+
+        // If zero was changed (some of the bits didn't match up), the passwords don't match.
+        return zero == 0;
     }
 
     private byte[] generateHash (char[] charsToHash, byte[] salt) {
         // Create KeySpec object from the characters and salt with the cost and key length constants.
-        final KeySpec spec = new PBEKeySpec(charsToHash, salt, COST, KEY_LENGTH);
+        final KeySpec spec = new PBEKeySpec(charsToHash, salt, COST, SIZE);
 
         try {
             // Generate the hash from the secret key factory.
